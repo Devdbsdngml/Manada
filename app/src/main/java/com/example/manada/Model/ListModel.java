0b5597f338b527093ee2f7b85e8ead173efa6db4
Devdbsdngml <wh9614y@gmail.com>
2019-11-06 20:12:48 +0900

    Query for sort

---
+++ app/src/main/java/com/example/manada/Model/ListModel.java
@@ -0,0 +1,12 @@
+package com.example.manada.Model;
+
+public class ListModel {
+
+    public ListModel() {
+
+    }
+
+    public String mycollege;
+    public String personnel;
+    public String yourcollege;
+}
